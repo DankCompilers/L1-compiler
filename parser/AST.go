2473a7e7b004e9c1c9627826cf63a21dede2ce22
ClaudeTheCazur <claudesonazurin2017@u.northwestern.edu>
2016-04-02 20:03:20 -0500

    starting file for ast helper functions

---
+++ parser/AST.go
@@ -0,0 +1,66 @@
+package L1ast
+
+type Node interface {
+	AppendChild(Node)
+}
+
+type ParseTreeNode struct {
+	NodeID   string
+	children []Node
+}
+
+func makeParseTreeNode(str string) Node {
+}
+
+func makeTokenNode(str string) Node {
+	b := MakePareTreeNode()
+}
+
+func makeSYSFUNCnode(str string) Node {
+}
+
+func makeCALLnode(str string) Node {
+
+}
+
+func makeVALTOMEMnode(mem Node, str2,str3,str4,str5,str6) Node {
+	return 0
+}
+
+func makeMEMTOREGnode() Node {
+
+}
+
+func makeMEMASOPINPUTnode() Node {
+
+}
+
+func makeASSINGNMENTnode() Node {
+
+}
+
+func makeGOTOnode() Node {
+
+}
+
+func makeJUMPCMPnode () Node {
+}
+
+func makeTailCallNode() Node {
+}
+
+func makeASSIGN_CMPnode() Node {
+
+}
+
+func makeInnerinstNode() Node {
+
+}
+
+func makeInstanceNode() Node {
+
+}
+
+func newFunctionNode() Node {
+
+}
