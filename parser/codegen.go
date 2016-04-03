d9df2f930454065d333366c612e83ed5a230e5b4
ClaudeTheCazur <claudesonazurin2017@u.northwestern.edu>
2016-04-03 03:45:32 -0500

    adding more case statements for Nodes

--- parser/codegen.go
+++ parser/codegen.go
@@ -7,6 +7,27 @@ import (
 	"strings"
 )
 
+var calleeArray = [...]string{"%rbx", "%rbp", "%r12", "%r13", "%r14", "%r15"}
+var callerArray = [...]string{"%rax", "%rcx", "%rdx", "%r8", "%r9", "%r10", "%r11"}
+
+eightBitEquiv := map[string]string{
+"r10" : "r10b",
+"r13" : "r13b",
+"r8"  : "r8b",
+"rbp" : "bpl",
+"rdi" : "dil",
+"r11" : "r11b",
+"r14" : "r14b",
+"r9"  : "r9b",
+"rbx" : "bl",
+"rdx" : "dl",
+"r12" : "r12b",
+"r15" : "r15b",
+"rax" : "al",
+"rcx" : "cl",
+"rsi" : "sil",
+}
+
 type codeGenerator interface {
 	beginCompiler(Node) error
 }
@@ -19,9 +40,6 @@ func L1toASMGenerator() *AsmCodeGenerator {
 	return &AsmCodeGenerator{}
 }
 
-var calleeArray = [...]string{"%rbx", "%rbp", "%r12", "%r13", "%r14", "%r15"}
-var callerArray = [...]string{"%rax", "%rcx", "%rdx", "%r8", "%r9", "%r10", "%r11"}
-
 func (c *AsmCodeGenerator) compNode(node Node) {
 
 	if node == nil {
@@ -31,13 +49,20 @@ func (c *AsmCodeGenerator) compNode(node Node) {
 	switch n := node.(type) {
 	case *ProgramNode:
 		for _, reg := range calleeArray {
-			c.writer.WriteString("push  %s", reg)
+			c.writer.WriteString("push  %%s \n", reg)
 		}
+
 		c.compNode(n.Child)
+
+		for reg :=len(calleeArray) - 1; i>=0; i-- {
+			regToWrite := calleeArray[i]
+			c.writer.WriteString("pop  %%s \n", regToWrite)
+		}
+		c.writer.WriteString("retq \n")
 
 	case *newAssignNode:
 		mem, value := n.children[:]
-		c.writer.WriteString("movq $%d, %s", value, mem)
+		c.writer.WriteString("movq $%d, %s \n", value, mem)
 
 	case *newOpNode:
 		mem, value := n.children[:]
@@ -48,7 +73,20 @@ func (c *AsmCodeGenerator) compNode(node Node) {
 			instruct := "salq"
 		}
 
-		c.writer.WriteString("%s %s, %s", instruct, value, mem)
+		if  numRep,err := strconv.Atoi(value); err == nil {
+			value = '$' + numRep
+		}
+
+		lowerReg = eightBitEquiv[mem]
+		c.writer.WriteString("%s %s, %s", instruct, value, lowerReg)
+
+	case *newCallNode:
+
+		u,NAT = n.children[:]
+		u := u.value
+		NAT := NAT
+
+
 
 	}
 }
