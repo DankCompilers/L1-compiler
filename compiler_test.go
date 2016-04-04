e3ea59f2c6474abea082029d0ec30b4118554658
ClaudeTheCazur <claudesonazurin2017@u.northwestern.edu>
2016-04-04 04:08:03 -0500

    changed name with standard

--- compiler_test.go
+++ compiler_test.go
@@ -1,19 +1,19 @@
 package l1compiler
 
+import (
+	"testing"
+)
 
-import ("testing")
+func TestL1Compiler(t *testing.T) {
+	c := NewCompiler()
 
+	err := c.Compile("01.L1")
+	if err != nil {
+		t.Fatal(err)
+	}
 
-func testL1Compiler(t *testing.T) {
-  c := NewCompiler()
-
-  err := c.Compile("01.L1")
-  if err != nil {
-    t.Fatal(err)
-  }
-
-  // err = c.GenerateCode()
-  // if err != nil {
-  //     t.Fatal(err)
-  // }
+	// err = c.GenerateCode()
+	// if err != nil {
+	//     t.Fatal(err)
+	// }
 }
