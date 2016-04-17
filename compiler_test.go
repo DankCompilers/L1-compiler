2d044aaa66d38a1686c9868dd9a2c3c867b2dfd6
ClaudeTheCazur <claudesonazurin2017@u.northwestern.edu>
2016-04-16 21:20:20 -0500

    Added flags and init function for command line args

--- compiler_test.go
+++ compiler_test.go
@@ -1,13 +1,22 @@
 package l1compiler
 
 import (
+	"flag"
 	"testing"
 )
 
+var fileToTest string
+
+func init() {
+	flag.StringVar(&fileToTest, "FileName", "none", "file name to be tested")
+}
+
 func TestL1Compiler(t *testing.T) {
+	flag.Parse()
 	c := NewCompiler()
 
-	err := c.Compile("02.L1")
+	err := c.Compile(fileToTest)
+	//	err := c.Compile("04.L1")
 	if err != nil {
 		t.Fatal(err)
 	}
